a1bfdb0d516449176478fa151f17b2e22e8a23bc
Chinonso Tony Okebugwu <nonnenzy01@gmail.com>
2023-08-28 09:19:13 +0100

    chores: add comments

--- monitoring/webhook.py
+++ monitoring/webhook.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 from django.core.mail import send_mail
 from .models import Transaction, User
 
+
+#Transaction amount exceeds the amount for a given tier.
 def evaluate_policy_5(transaction):
     tier_amounts = {
         "bronze": 1000,
@@ -15,6 +17,7 @@ def evaluate_policy_5(transaction):
     return False
 
 
+#Transaction is happening between a regular user and previously flagged user.
 def evaluate_policy_4(transaction):
     regular_user = User.objects.get(id=transaction.user_id)
     flagged_users = User.objects.filter(flagged=True)
@@ -23,12 +26,14 @@ def evaluate_policy_4(transaction):
         return True
     return False
 
+# Transaction is attributed to a new user.
 def evaluate_policy_3(transaction):
     user_transactions = Transaction.objects.filter(user=transaction.user)
     if user_transactions.count() == 1:
         return True
     return False
 
+# Transaction from a particular user occurs within a timing window of less than 1 minute.
 def evaluate_policy_2(transaction):
     prev_transaction = Transaction.objects.filter(
         user=transaction.user,
@@ -40,12 +45,13 @@ def evaluate_policy_2(transaction):
         return True
     return False
 
+# The transaction amount is greater than 5,000,000.
 def evaluate_policy(transaction):
-    # Implement your policy evaluation logic here
     if transaction.amount < 5000000:
         return True
     return False
 
+# send email
 def send_notification_email(transaction):
     subject = "Transaction Alert"
     message = f"A transaction of amount ${transaction.amount} requires attention."
@@ -54,6 +60,7 @@ def send_notification_email(transaction):
 
     send_mail(subject, message, from_email, recipient_list)
 
+#process transaction
 def process_transaction(transaction_id):
     try:
         transaction = Transaction.objects.get(id=transaction_id)
